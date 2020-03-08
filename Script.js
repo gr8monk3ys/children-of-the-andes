60c80294736bcb0c105611686b7f2034cc76ce1c
Lorenzo Scaturchio <lorenzosca7@gmail.com>
2020-03-07 19:00:41 -0800

    implemented to master

---
+++ Script.js
@@ -0,0 +1,26 @@
+function callFullPage(){ 
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true,
+        controlArrows: false,
+        slidesNavigation: true,
+        navigation: true,
+        navigationTooltips: ['Home', 'About Us', 'Schedule', 'Flights', 'Contact Us'],
+        showActiveTooltip: true, 
+        scrollingSpeed: 1250,
+      });
+}
+
+function callTypeForm(){
+    var qs,js,q,s,d=document, 
+    gi=d.getElementById, ce=d.createElement, 
+    gt=d.getElementsByTagName, id="typef_orm", 
+    b="https://embed.typeform.com/"; 
+    if(!gi.call(d,id)) { 
+        js=ce.call(d,"script"); 
+        js.id=id; js.src=b+"embed.js"; 
+        q=gt.call(d,"script")[0]; 
+        q.parentNode.insertBefore(js,q) 
+    } 
+}
